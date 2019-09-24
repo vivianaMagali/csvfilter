c6d9ca235c7f9e858033aab5fea4caf005250465
Viviana Benitez <magi61012@gmail.com>
2019-09-24 18:18:03 +0100

    green: tax_fields_are_mutually_exclusive

--- src/test/kotlin/csvfilter/CsvFilterShould.kt
+++ src/test/kotlin/csvfilter/CsvFilterShould.kt
@@ -15,5 +15,14 @@ class CsvFilterShould {
         assertThat(result).isEqualTo(listOf(headerLine, invoiceLine))
 
     }
+    @Test
+    fun tax_fields_are_mutually_exclusive(){
+        val headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF _cliente, NIF _cliente"
+        val invoiceLine = "1,02/05/2019,1000,810,19,8,ACER Laptop,B76430134"
+
+        val result = CsvFilter().filter(listOf(headerLine, invoiceLine))
+
+        assertThat(result).isEqualTo(listOf(headerLine))
+    }
 
 }
